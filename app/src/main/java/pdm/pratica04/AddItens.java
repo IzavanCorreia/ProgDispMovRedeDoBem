72db665fb8da962e53102a31a853c38f37a0bcbd
Lucas Meira <lucasrenaa1996@gmail.com>
2023-05-07 19:17:06 -0300

    projeto mobile primeira entrega

---
+++ app/src/main/java/pdm/pratica04/AddItens.java
@@ -0,0 +1,14 @@
+package pdm.pratica04;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddItens extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_itens);
+    }
+}
