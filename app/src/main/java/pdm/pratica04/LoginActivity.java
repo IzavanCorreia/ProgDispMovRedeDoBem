d03d295f27b485506963eb4bd99a26a227d2bfe0
Izavan Correia <isc1@discente.ifpe.edu.br>
2023-06-01 20:05:35 -0300

    Tela de Login FRONT

--- app/src/main/java/pdm/pratica04/LoginActivity.java
+++ app/src/main/java/pdm/pratica04/LoginActivity.java
@@ -1,14 +1,55 @@
 package pdm.pratica04;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText editTextEmail;
+    private EditText editTextPassword;
+    private Button buttonLogin;
+    private TextView textViewRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        // Referenciar os elementos da interface do usuário
+        editTextEmail = findViewById(R.id.editTextEmail);
+        editTextPassword = findViewById(R.id.editTextPassword);
+        buttonLogin = findViewById(R.id.buttonLogin);
+        textViewRegister = findViewById(R.id.textViewRegister);
+
+        // Definir um listener para o botão de login
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = editTextEmail.getText().toString();
+                String password = editTextPassword.getText().toString();
+
+                // Aqui você pode adicionar a lógica de validação do login
+                // Por exemplo, verificar se o email e a senha são válidos
+
+                // Exemplo simples para exibir uma mensagem de sucesso no login
+                Toast.makeText(LoginActivity.this, "Login successful!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // Definir um listener para o link de registro
+        textViewRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Abre a tela de registro
+                Intent intent = new Intent(LoginActivity.this, RegistroActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
